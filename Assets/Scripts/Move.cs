0d9712a2c0d16d25f2ebf3b45f8715a4ca788bb4
十木 <yangwx0905@sina.com>
2018-12-08 02:29:49 +0800

    人物bug修改

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -21,6 +21,7 @@ public class Move : MonoBehaviour
 	[SerializeField]
 	private float switchTime;
 	private Rigidbody rb;
+	[SerializeField]
 	private bool isGround;
 	private Animator animator;
 	//步行动画的切换
@@ -38,18 +39,15 @@ public class Move : MonoBehaviour
 
 	private void Update() 
 	{
-		if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
+		if (Input.GetKey(KeyCode.A))
 		{
-			if (Input.GetKey(KeyCode.A))
-			{
-				transform.rotation = Quaternion.Euler(0, -90, 0);
-				Walk(-1);
-			}
-			else
-			{
-				transform.rotation = Quaternion.Euler(0, 90, 0);
-				Walk(1);
-			}
+			transform.rotation = Quaternion.Euler(0, -90, 0);
+			Walk(-1);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.rotation = Quaternion.Euler(0, 90, 0);
+			Walk(1);
 		}
 		if (Input.GetKey(KeyCode.W))
 		{
@@ -79,17 +77,17 @@ public class Move : MonoBehaviour
 		{
 			animator.SetFloat("Blend", walkAnimate);
 		}
-		float velocity_y = rb.velocity.y;
-		rb.velocity = new Vector3(dir, velocity_y, 0) * speed;
+		rb.velocity = new Vector3(dir * speed, rb.velocity.y, 0);
 		
 	}
 
 	private void Jump()
 	{
-		if (playerState != State.Jump && isGround)
+		if (isGround)
 		{
 			animator.SetFloat("Blend", 0.33f);
-			rb.AddForce(new Vector3(0, jumpForce, 0));
+			rb.AddForce(Vector3.up * jumpForce);
+			Debug.Log(rb.velocity);
 			isGround = false;
 			animator.SetFloat("Blend", 0);
 			playerState = State.Jump;
@@ -101,7 +99,7 @@ public class Move : MonoBehaviour
 		if (other.transform.tag == "Floor")
 		{
 			isGround = true;
-			if (playerState == State.Jump)
+			if (playerState != State.Walk)
 			{
 				playerState = State.Idle;
 				animator.SetFloat("Blend", 0.33f);
