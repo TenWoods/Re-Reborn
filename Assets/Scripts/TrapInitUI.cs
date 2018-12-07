9b8df8de2e525eebce0ae19050ed3bbc308ef8ae
王啸川 <m15048619182@163.com>
2018-12-08 01:10:27 +0800

    << 陷阱测试

---
+++ Assets/Scripts/TrapInitUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TrapInitUI : TriggerThing {
+	public GameObject canvas;
+	private Text thisText;
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			if(!isFirstTrig)
+			{
+				Debug.Log("should init a UI");
+				canvas.SetActive(true);
+			}
+		}
+	}
+	/// <summary>
+	/// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))		
+		{
+			if(!isFirstTrig)
+			{
+				//Debug.Log("should init a UI");
+				canvas.SetActive(false);
+			}
+		}
+	}
+	
+}
---
+++ Assets/Scripts/TriggerThing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerThing : MonoBehaviour {
+	protected GameObject canvas;
+
+	protected bool isFirstTrig = false;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		//canvas = GameObject.Find("")
+	}
+}
