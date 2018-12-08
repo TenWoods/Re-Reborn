4b12f817fa110a216241d29ba2c445fe0c1175bb
十木 <yangwx0905@sina.com>
2018-12-08 10:45:31 +0800

    结束相机

---
+++ Assets/Scripts/BurnController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnController : MonoBehaviour 
+{
+	[SerializeField]
+	private float burnSpeed;
+	private Material burnMaterial;
+	private float burnAmount = 0;
+	public bool burnfinish = false;
+	public bool burnStart = false;
+
+	private void Start() 
+	{
+		burnMaterial = GetComponent<Renderer>().material;
+		if (burnMaterial == null)
+		{
+			this.enabled = false;
+		}
+	}
+
+	private void Update() 
+	{
+		if (!burnStart)
+		{
+			return;
+		}
+		burnAmount = Mathf.Lerp(burnAmount, 1, Time.deltaTime * burnSpeed);
+		burnMaterial.SetFloat("_BurnAmount", burnAmount);
+		if (Mathf.Abs(burnAmount - 1) <= 0.1f)
+		{
+			burnfinish = true;
+			this.enabled = false;
+		}
+	}
+}
