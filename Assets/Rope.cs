3df426ddc9bcd3ec7da1957ad13f20e6eaac072f
王啸川 <m15048619182@163.com>
2018-12-08 08:51:33 +0800

    << 加了个绳子

---
+++ Assets/Rope.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour {
+
+   //存储RopeParent的Rigibody组件
+    internal Rigidbody RBody;
+	// Use this for initialization
+	internal void Start () {
+        //给RopeParent添加Rigibody组件
+        this.gameObject.AddComponent<Rigidbody>();
+        //获取RopeParent的Rigibody组件并赋值给RBody
+        this.RBody = this.gameObject.GetComponent<Rigidbody>();
+        this.RBody.isKinematic = true;
+        //RopeParent中子物体的数量
+        //给每一个子物体都加上Hinge Joint组件
+        int childcount = this.transform.childCount;
+        for (int i = 0; i < childcount;i++) {
+            Transform t = this.transform.GetChild(i);
+            t.gameObject.AddComponent<HingeJoint>();
+ 
+            HingeJoint hinge = t.gameObject.GetComponent<HingeJoint>();
+            hinge.connectedBody = i == 0 ? this.RBody : this.transform.GetChild(i - 1).GetComponent<Rigidbody>();
+            hinge.useSpring = true;
+            hinge.enableCollision = true;
+        }
+	}	
+}
